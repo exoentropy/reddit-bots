5aba8a7a82fa2a793e74a1c3ee41bcd9fd5e9244
Scottie Enriquez <scottie@scott-enriquez.net>
2014-02-16 21:21:54 -0900

    #1: Add make-sfw bot

---
+++ make-sfw.py
@@ -0,0 +1,17 @@
+import sys, praw
+
+userAgent = """make-sfw.py (PRAW) // removes posts containing black listed words and phrases // https://github.com/scottenriquez/reddit-bots // /u/Exoentropy"""
+
+reddit = praw.Reddit(userAgent)
+reddit.login()
+
+#make black list
+blackList = []
+for nsfw in sys.stdin:
+	blackList.append(nsfw.strip())
+
+#parse user's history and delete posts containing an post from the black list
+for post in reddit.user.get_overview(limit = None):
+    for nsfw in blackList:
+    	if(nsfw in str(post)):
+    		post.delete()
